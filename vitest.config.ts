77b1951a4c672158c7c27edf15b78562d1618ec7
rainbowatcher <rainbow-w@qq.com>
2024-08-02 17:21:00 +0800

    test: add coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
         },
     },
     test: {
+        coverage: {
+            exclude: ["packages/{cross-release-cli,cross-bump}/src/index.ts", "**/types.ts"],
+            include: ["packages/**/src/**/*.{ts,tsx}"],
+        },
         name: "cross-release",
     },
 })
