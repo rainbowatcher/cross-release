7700b23b3e1c112e26f2ca98932fe0c0039a99d4
rainbowatcher <rainbow-w@qq.com>
2024-08-05 11:29:15 +0800

    test(cross-release-cli): add config test cases

---
+++ packages/cross-release-cli/src/util/config.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it } from "vitest"
+import { CONFIG_DEFAULT } from "../constants"
+import { resolveAltOptions } from "./config"
+import type { ReleaseOptions } from "../types"
+
+const options: ReleaseOptions = {
+    commit: true,
+    config: "",
+    cwd: "",
+    dry: false,
+    excludes: [],
+    main: "javascript",
+    push: false,
+    recursive: false,
+    showVersion: false,
+    tag: true,
+    version: "",
+    yes: false,
+}
+
+describe("resolveAltOptions", () => {
+    it("should return the default value if value is undefined", () => {
+        // @ts-expect-error param type incompatible
+        const result = resolveAltOptions(options, "key")
+        expect(result).toEqual({})
+    })
+
+    it("should return default if value is true", () => {
+        const result = resolveAltOptions(options, "commit", CONFIG_DEFAULT.commit)
+        expect(result).toEqual(CONFIG_DEFAULT.commit)
+    })
+
+    it("should return {} if value is false", () => {
+        const result = resolveAltOptions(options, "push", CONFIG_DEFAULT.push)
+        expect(result).toEqual({})
+    })
+})
