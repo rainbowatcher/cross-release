534e097c7ddd5a066b1cd86c3ac6a0f6cf72db1d
rainbowatcher <rainbow-w@qq.com>
2024-08-05 11:22:48 +0800

    chore(cross-bump): simplify exports

--- packages/cross-bump/src/index.ts
+++ packages/cross-bump/src/index.ts
@@ -1,42 +1,5 @@
-import { parse } from "semver"
-import { findProjectFiles } from "./project"
-import {
-    getJavaProjectVersion,
-    getJSProjectVersion,
-    getNextVersions,
-    getProjectVersion,
-    getRustProjectVersion,
-    isVersionValid,
-    upgradeCargoVersion,
-    upgradePackageVersion,
-    upgradePomVersion,
-    upgradeProjectVersion,
-} from "./version"
-import type { ProjectCategory, ProjectFile, ProjectFileName } from "./project"
-
-// version related
-export {
-    getJavaProjectVersion,
-    getJSProjectVersion,
-    getNextVersions,
-    getProjectVersion,
-    getRustProjectVersion,
-    isVersionValid,
-    upgradeCargoVersion,
-    upgradePackageVersion,
-    upgradePomVersion,
-    upgradeProjectVersion,
-}
-
-// re-export from semver
-export { parse as parseVersion }
-
-// project related
-export { findProjectFiles }
-
-// types
-export type {
-    ProjectCategory,
-    ProjectFile,
-    ProjectFileName,
-}
+export * from "./ignore"
+export * from "./project"
+export * from "./util"
+export * from "./version"
+export { parse as parseVersion } from "semver"
