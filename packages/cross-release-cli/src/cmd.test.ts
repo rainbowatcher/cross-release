ca9d6e688b6d4c10557d8c32e6e6755c3d9041a4
rainbowatcher <rainbow-w@qq.com>
2024-08-06 11:14:51 +0800

    test: should consider user config options

--- packages/cross-release-cli/src/cmd.test.ts
+++ packages/cross-release-cli/src/cmd.test.ts
@@ -1,19 +1,21 @@
 import process from "node:process"
 import { getGitignores } from "cross-bump"
 import { describe, expect, it } from "vitest"
-import { resolveOptions } from "./cmd"
+import { loadUserConfig, resolveOptions } from "./cmd"
 import { CONFIG_DEFAULT } from "./constants"
 import type { CAC } from "cac"
 
 
-describe("arg parse", () => {
+describe.concurrent("arg parse", () => {
     it("should parse args", async () => {
         const cli = { args: [], options: {} }
         const opts = await resolveOptions(cli as unknown as CAC)
+        const userConfig = await loadUserConfig({})
         expect(opts).toEqual({
             ...CONFIG_DEFAULT,
             cwd: process.cwd(),
-            excludes: [...await getGitignores(process.cwd())],
+            excludes: [...await getGitignores(process.cwd()), ...userConfig.excludes],
+            version: undefined,
         })
     })
 })
