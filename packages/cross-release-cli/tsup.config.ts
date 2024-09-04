bfb2af716ef2e6753e6133c568fef7797805329f
rainbowatcher <rainbow-w@qq.com>
2024-09-04 22:57:09 +0800

    build(cross-release-cli): custom dts build logic and add skipNodeModulesBundle

--- packages/cross-release-cli/tsup.config.ts
+++ packages/cross-release-cli/tsup.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
     clean: true,
-    dts: true,
+    dts: {
+        entry: [
+            "src/app.ts",
+            "src/index.ts",
+            "src/types.ts",
+        ],
+    },
     entry: ["src/index.ts", "src/app.ts"],
     format: ["esm"],
+    skipNodeModulesBundle: true,
 })
