cd872822184172b839d84021c484e80183a30b8a
rainbowatcher <rainbow-w@qq.com>
2025-04-04 12:00:28 +0800

    feat: refactor CLI options and enhance user configuration handling
    - Update test cases to use argvToReleaseOptions instead of toCliReleaseOptions
    - Refactor CLI parsing to use cac library instead of commander
    - Add new command options related to commit and push behaviors
    - Introduce functions for resolving and validating application options
    - Improve handling of user configurations and merging with defaults
    - Enhance debugging capabilities with more detailed logs
    
    Signed-off-by: rainbowatcher <rainbow-w@qq.com>

--- packages/cross-release-cli/src/cli.test.ts
+++ packages/cross-release-cli/src/cli.test.ts
@@ -1,85 +1,295 @@
-import { describe, expect, it } from "vitest"
-import { createCliProgram, toCliReleaseOptions } from "./cli"
+import { isFileSync } from "@rainbowatcher/fs-extra"
+import { toAbsolute } from "@rainbowatcher/path-extra"
+import { getGitignores } from "cross-bump"
+import { loadConfig } from "unconfig"
+import {
+    describe, expect, it, vi,
+} from "vitest"
+import { argvToReleaseOptions, createCliProgram, resolveAppOptions } from "./cli"
 import { CONFIG_DEFAULT } from "./constants"
 
+
 function parseArg(...args: string[]) {
-    return createCliProgram().parse(["", "", ...args])
+    return createCliProgram(["", "", ...args])
 }
 
-describe.concurrent("arg parse", () => {
-    it("default cli parse", () => {
-        const cli = createCliProgram()
-        expect(cli.args).toStrictEqual([])
-        expect(cli.opts()).toStrictEqual({
-            ...CONFIG_DEFAULT,
-            commit: true,
-            push: true,
-            tag: true,
-        })
-    })
-
-    it("should parse args", () => {
-        const cli = createCliProgram().parse([])
-        expect(cli.opts()).toStrictEqual({
-            ...CONFIG_DEFAULT,
-            commit: true,
-            push: true,
-            tag: true,
-        })
-    })
-})
-
-
-describe.concurrent("toReleaseOptions", () => {
+describe("toReleaseOptions", () => {
     it("execute", () => {
         const cli = parseArg("-x", "echo hello")
-        const ro = toCliReleaseOptions(cli)
+        const ro = argvToReleaseOptions(cli)
         expect(ro.execute).toStrictEqual(["echo hello"])
     })
 
     it("multi execute", () => {
-        const cli = parseArg("-x", "echo hello", "echo bye")
-        const ro = toCliReleaseOptions(cli)
+        const cli = parseArg("-x", "echo hello", "-x", "echo bye")
+        const ro = argvToReleaseOptions(cli)
         expect(ro.execute).toStrictEqual(["echo hello", "echo bye"])
     })
 
     it("multi execute with multi option", () => {
         const cli = parseArg("-x", "echo hello", "-x", "echo bye")
-        const ro = toCliReleaseOptions(cli)
+        const ro = argvToReleaseOptions(cli)
         expect(ro.execute).toStrictEqual(["echo hello", "echo bye"])
     })
 
     it("config", () => {
         const config = "fixture/package.json"
         const cli = parseArg("-c", config)
-        const ro = toCliReleaseOptions(cli)
+        const ro = argvToReleaseOptions(cli)
         expect(ro.config).toBe(config)
     })
 
     it("cwd", () => {
         const cli = parseArg("--cwd", "fixture")
-        const ro = toCliReleaseOptions(cli)
+        const ro = argvToReleaseOptions(cli)
         expect(ro.cwd).toBe("fixture")
     })
 
     it("cwd and config", () => {
         const config = "fixture/package.json"
         const cli = parseArg("-c", config, "--cwd", "fixture")
-        const ro = toCliReleaseOptions(cli)
+        const ro = argvToReleaseOptions(cli)
         expect(ro.config).toBe(config)
     })
 
     it("yes and push", () => {
         const cli = parseArg("-y", "--no-push")
-        const ro = toCliReleaseOptions(cli)
+        const ro = argvToReleaseOptions(cli)
         expect(ro.push).toBeFalsy()
         expect(ro.yes).toBeTruthy()
     })
 
     it("yes", () => {
         const cli = parseArg("-y")
-        const ro = toCliReleaseOptions(cli)
+        const ro = argvToReleaseOptions(cli)
         expect(ro.yes).toBeTruthy()
-        expect(ro.push).toBeTruthy()
+    })
+})
+
+
+describe("resolveAppOptions", () => {
+    vi.mock("cross-bump")
+    vi.mocked(getGitignores).mockReturnValue(new Set())
+
+    it("should props equal to config default when pass nothing", () => {
+        const cli = parseArg()
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            config: undefined,
+            version: undefined,
+        })
+    })
+
+    it("should props equal to config default when pass yes", () => {
+        const cli = parseArg("-y")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            config: undefined,
+            version: undefined,
+            yes: true,
+        })
+    })
+
+    it("should props equal to config default when pass --push", () => {
+        const cli = parseArg("--push")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            config: undefined,
+            version: undefined,
+        })
+    })
+
+    it("should props equal to config default when pass --no-push", () => {
+        const cli = parseArg("--no-push")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            config: undefined,
+            push: false,
+            version: undefined,
+        })
+    })
+
+    it("should props equal to config default when pass --no-tag", () => {
+        const cli = parseArg("--no-tag")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            config: undefined,
+            tag: false,
+            version: undefined,
+        })
+    })
+
+    it("should props equal to config default when pass --no-commit", () => {
+        const cli = parseArg("--no-commit")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            commit: false,
+            config: undefined,
+            version: undefined,
+        })
+    })
+
+    it("should works when pass --no-push --no-tag --no-commit", () => {
+        const cli = parseArg("--no-push", "--no-tag", "--no-commit")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            commit: false,
+            config: undefined,
+            push: false,
+            tag: false,
+            version: undefined,
+        })
+    })
+
+    it("should works when passing --main javascript", () => {
+        const cli = parseArg("--main", "javascript")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            config: undefined,
+            main: "javascript",
+            version: undefined,
+        })
+    })
+
+    it("should works when passing --main rust", () => {
+        const cli = parseArg("--main", "rust")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            config: undefined,
+            main: "rust",
+            version: undefined,
+        })
+    })
+
+    it("should works when passing --main java", () => {
+        const cli = parseArg("--main", "java")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            config: undefined,
+            main: "java",
+            version: undefined,
+        })
+    })
+
+    it("should works when passing -r", () => {
+        const cli = parseArg("-r")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            config: undefined,
+            recursive: true,
+            version: undefined,
+        })
+    })
+
+    it("should works when passing --config", () => {
+        vi.mock("unconfig")
+        vi.mock("@rainbowatcher/fs-extra")
+        const mockConfig = {
+            config: { main: "java" },
+            sources: ["cross-release.config.ts"],
+        }
+        vi.mocked(loadConfig.sync).mockReturnValue(mockConfig)
+        vi.mocked(isFileSync).mockReturnValue(true)
+
+        const cli = parseArg("--config", "cross-release.config.ts")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            config: toAbsolute("cross-release.config.ts"),
+            main: "javascript",
+            version: undefined,
+        })
+    })
+
+    it("should work when passing --commit.signoff", () => {
+        const cli = parseArg("--commit.signoff")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            commit: {
+                signoff: true,
+                stageAll: false,
+                template: "chore: release v%s",
+                verify: true,
+            },
+            config: undefined,
+            main: "javascript",
+            version: undefined,
+        })
+    })
+
+    it("should work when passing --no-commit.signoff", () => {
+        const cli = parseArg("--no-commit.signoff")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            commit: {
+                signoff: false,
+                stageAll: false,
+                template: "chore: release v%s",
+                verify: true,
+            },
+            config: undefined,
+            main: "javascript",
+            version: undefined,
+        })
+    })
+
+    it("should work when passing --commit.stageAll", () => {
+        const cli = parseArg("--commit.stageAll")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            commit: {
+                signoff: true,
+                stageAll: true,
+                template: "chore: release v%s",
+                verify: true,
+            },
+            config: undefined,
+            main: "javascript",
+            version: undefined,
+        })
+    })
+
+    it("should work when passing --push.followTags", () => {
+        const cli = parseArg("--push.followTags")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            config: undefined,
+            main: "javascript",
+            push: {
+                ...CONFIG_DEFAULT.push,
+                followTags: true,
+            },
+            version: undefined,
+        })
+    })
+
+    it("should throw when passing --push.followTags and --push.followTags", () => {
+        const cli = parseArg("--push.followTags", "--push.followTags")
+        const opts = resolveAppOptions(cli)
+        expect(opts).toStrictEqual({
+            ...CONFIG_DEFAULT,
+            config: undefined,
+            main: "javascript",
+            push: {
+                ...CONFIG_DEFAULT.push,
+                followTags: true,
+            },
+            version: undefined,
+        })
     })
 })
