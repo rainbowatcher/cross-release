843a0020b994255a2927d854a03b6596eb23cc72
rainbowatcher <rainbow-w@qq.com>
2025-08-15 09:36:38 +0800

    build: remove the clean and dts options from tsdown config
    Signed-off-by: rainbowatcher <rainbow-w@qq.com>

--- packages/cross-bump/tsdown.config.ts
+++ packages/cross-bump/tsdown.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "tsdown"
 
 export default defineConfig({
-    clean: true,
-    dts: {
-        // isolatedDeclarations: true,
-    },
     entry: ["src/index.ts"],
     skipNodeModulesBundle: true,
 })
